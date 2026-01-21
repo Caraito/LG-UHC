41f1abbc2746ddc62e6a34cf44a9a0b9d09debb0
Caraito <caraito.jeu.video@gmail.com>
2026-01-21 15:51:06 +0100

    Update scoreboard display: show alive player count and world border size

--- src/main/java/fr/caraito/lguhc/managers/ScoreboardManager.java
+++ src/main/java/fr/caraito/lguhc/managers/ScoreboardManager.java
@@ -3,6 +3,7 @@ package fr.caraito.lguhc.managers;
 import fr.caraito.lguhc.Main;
 import fr.caraito.lguhc.roles.LGRole;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
@@ -24,14 +25,25 @@ public class ScoreboardManager {
             time = Main.getInstance().getGameTask().getSeconds();
         }
 
-        obj.getScore("§7----------------").setScore(7);
-        obj.getScore("§fTemps: §e" + formatTime(time)).setScore(6);
-        obj.getScore("§fJoueurs: §a" + Bukkit.getOnlinePlayers().size()).setScore(5);
+        // Calcul des joueurs encore en vie (ceux qui sont en mode Survie)
+        long aliveCount = Bukkit.getOnlinePlayers().stream()
+                .filter(p -> p.getGameMode() == GameMode.SURVIVAL)
+                .count();
+
+        // Récupération de la bordure (rayon de la bordure)
+        int border = (int) player.getWorld().getWorldBorder().getSize() / 2;
+
+        obj.getScore("§7----------------").setScore(8);
+        obj.getScore("§fTemps: §e" + formatTime(time)).setScore(7);
+        obj.getScore("§fJoueurs en vie: §a" + aliveCount).setScore(6);
+        obj.getScore("§fBordure: §b±" + border).setScore(5);
 
         String roleName = "§7Attente...";
         LGRole role = Main.getInstance().getRoleManager().getRole(player.getUniqueId());
         if (role != null) roleName = "§e" + role.getName();
         obj.getScore("§fRôle: " + roleName).setScore(4);
+
+        obj.getScore("§7---------------- ").setScore(3);
 
         player.setScoreboard(board);
     }
